62fc230a65efbb076b0f5974baa98c3b88e1d016
Librariann <okpc0306@naver.com>
2023-11-12 21:29:38 +0900

    [commit] add jwt folder, add hashPassword, checkPassword

--- src/user/entity/user.entity.ts
+++ src/user/entity/user.entity.ts
@@ -1,19 +1,43 @@
 import { CoreEntity } from 'src/common/entity/core.entity';
-import { Column, Entity } from 'typeorm';
+import * as bcrypt from 'bcrypt';
+import { BeforeInsert, BeforeUpdate, Column, Entity } from 'typeorm';
 import { InputType, ObjectType, Field } from '@nestjs/graphql';
-import { IsString, IsEmail } from 'class-validator';
+import { IsString, IsEmail, IsNotEmpty } from 'class-validator';
+import { InternalServerErrorException } from '@nestjs/common';
 
 @InputType('UserInputType', { isAbstract: true })
 @ObjectType()
 @Entity()
 export class User extends CoreEntity {
-  @Column({ unique: true })
+  @Column({ unique: true, nullable: false })
   @Field(() => String)
+  @IsNotEmpty()
   @IsEmail()
   email: string;
 
   @Column({ select: false })
   @Field(() => String)
+  @IsNotEmpty()
   @IsString()
   password: string;
+
+  @BeforeInsert()
+  @BeforeUpdate()
+  async hashPassword(): Promise<void> {
+    if (this.password) {
+      try {
+        this.password = await bcrypt.hash(this.password, 10);
+      } catch (e) {
+        throw new InternalServerErrorException();
+      }
+    }
+  }
+
+  async checkPassword(aPassword: string): Promise<boolean> {
+    try {
+      return await bcrypt.compare(aPassword, this.password);
+    } catch (e) {
+      throw new InternalServerErrorException();
+    }
+  }
 }
