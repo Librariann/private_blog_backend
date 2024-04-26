b91fbded15f8ab83177851ee29c049380244be15
Librariann <okpc0306@naver.com>
2024-04-26 10:00:00 +0900

    [commit] add hashtag.resolver.ts

--- src/hashtag/hashtag.resolver.ts
+++ src/hashtag/hashtag.resolver.ts
@@ -1,8 +1,20 @@
-import { Resolver } from '@nestjs/graphql';
+import { Query, Mutation, Resolver } from '@nestjs/graphql';
 import { Hashtag } from './entity/hashtag.entity';
-// import { HashtagService } from './hashtag.service';
+import { GetHashTagOutput } from './dto/get-hashtag-list.dto';
+import { HashtagService } from './hashtag.service';
+import { CreateHashTagOutput } from './dto/create-hashtag.dto';
 
 @Resolver(() => Hashtag)
 export class HashtagResolver {
-  // constructor(private readonly hashtagService: HashtagService)
+  constructor(private readonly hashtagService: HashtagService) {}
+
+  @Mutation(() => CreateHashTagOutput)
+  createHashTag(): Promise<CreateHashTagOutput> {
+    return this.hashtagService.createHashTag();
+  }
+
+  @Query(() => GetHashTagOutput)
+  getHashTagList(): Promise<GetHashTagOutput> {
+    return this.hashtagService.getHashTagList();
+  }
 }
