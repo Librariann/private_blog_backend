7f1c61a1f4b645abab65f65d026097ee3632b950
Librariann <okpc0306@naver.com>
2024-04-27 10:00:00 +0900

    [commit] add hashtag.service.ts

--- src/hashtag/hashtag.service.ts
+++ src/hashtag/hashtag.service.ts
@@ -1,4 +1,42 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Hashtag } from './entity/hashtag.entity';
+import { Repository } from 'typeorm';
+import { GetHashTagOutput } from './dto/get-hashtag-list.dto';
+import { ok } from 'assert';
+import { error } from 'console';
+import { CreateHashTagOutput } from './dto/create-hashtag.dto';
 
 @Injectable()
-export class HashtagService {}
+export class HashtagService {
+  constructor(
+    @InjectRepository(Hashtag)
+    private readonly hastag: Repository<Hashtag>,
+  ) {}
+
+  async getHashTagList(): Promise<GetHashTagOutput> {
+    try {
+      return {
+        ok: true,
+      };
+    } catch (e) {
+      return {
+        ok: false,
+        error: '해시태그를 가져올 수 없습니다. 관리자에게 문의해주세요.',
+      };
+    }
+  }
+
+  async createHashTag(): Promise<CreateHashTagOutput> {
+    try {
+      return {
+        ok: true,
+      };
+    } catch (e) {
+      return {
+        ok: false,
+        error: '해시태그를 생성 할 수 없습니다. 관리자에게 문의해주세요',
+      };
+    }
+  }
+}
