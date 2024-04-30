a20492ee1734ce2ab0417681f7d8521710c32114
Librariann <okpc0306@naver.com>
2024-04-30 10:00:00 +0900

    [commit] edit create-hashtag.dto.ts

--- src/hashtag/dto/create-hashtag.dto.ts
+++ src/hashtag/dto/create-hashtag.dto.ts
@@ -1,5 +1,15 @@
-import { ObjectType } from '@nestjs/graphql';
+import { Field, InputType, Int, ObjectType, PickType } from '@nestjs/graphql';
 import { Output } from 'src/common/dto/output.dto';
+import { Hashtag } from '../entity/hashtag.entity';
+
+@InputType()
+export class CreateHashTagInput extends PickType(Hashtag, ['hashtag']) {
+  @Field(() => Int)
+  postId?: number;
+}
 
 @ObjectType()
-export class CreateHashTagOutput extends Output {}
+export class CreateHashTagOutput extends Output {
+  @Field(() => Int)
+  hashtagId?: number;
+}
