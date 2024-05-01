e61cda7efb0c735f2f0172a950967cca015ac588
Librariann <okpc0306@naver.com>
2024-05-01 10:00:00 +0900

    [commit] edit hashtag.entity.ts

--- src/hashtag/entity/hashtag.entity.ts
+++ src/hashtag/entity/hashtag.entity.ts
@@ -1,16 +1,22 @@
-import { Field, ObjectType } from '@nestjs/graphql';
-import { IsString } from 'class-validator';
+import { Field, InputType, ObjectType } from '@nestjs/graphql';
+import { IsNumber, IsString } from 'class-validator';
 import { CoreEntity } from 'src/common/entity/core.entity';
 import { Post } from 'src/post/entity/post.entity';
 import { Column, Entity, ManyToOne } from 'typeorm';
 
+@InputType('HashtagInputType', { isAbstract: true })
 @ObjectType()
 @Entity()
 export class Hashtag extends CoreEntity {
   @Column({ nullable: false })
   @Field(() => String)
   @IsString()
-  name: string;
+  hashtag: string;
+
+  @Column({ nullable: true })
+  @Field(() => Number, { nullable: true })
+  @IsNumber()
+  postId: number;
 
   @ManyToOne(() => Post, (post) => post.hashtags, {
     onDelete: 'SET NULL',
