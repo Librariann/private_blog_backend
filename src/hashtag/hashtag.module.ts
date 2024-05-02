d145119d812d919d440ff51e2138797e666e257d
Librariann <okpc0306@naver.com>
2024-05-02 10:00:00 +0900

    [commit] edit hashtag.module.ts

--- src/hashtag/hashtag.module.ts
+++ src/hashtag/hashtag.module.ts
@@ -1,10 +1,12 @@
-import { Module, Post } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { HashtagService } from './hashtag.service';
 import { HashtagResolver } from './hashtag.resolver';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Hashtag } from './entity/hashtag.entity';
+import { Post } from 'src/post/entity/post.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Post])],
+  imports: [TypeOrmModule.forFeature([Hashtag, Post])],
   providers: [HashtagResolver, HashtagService],
   exports: [HashtagService],
 })
