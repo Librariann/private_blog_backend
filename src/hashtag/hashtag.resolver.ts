4ec46ace89ba33130c0f650db392ed0c039d38c6
Librariann <okpc0306@naver.com>
2024-05-03 10:00:00 +0900

    [commit] edit hashtag.resolver.ts

--- src/hashtag/hashtag.resolver.ts
+++ src/hashtag/hashtag.resolver.ts
@@ -1,16 +1,29 @@
-import { Query, Mutation, Resolver } from '@nestjs/graphql';
+import { Query, Mutation, Resolver, Args, Int } from '@nestjs/graphql';
 import { Hashtag } from './entity/hashtag.entity';
 import { GetHashTagOutput } from './dto/get-hashtag-list.dto';
 import { HashtagService } from './hashtag.service';
-import { CreateHashTagOutput } from './dto/create-hashtag.dto';
+import {
+  CreateHashTagInput,
+  CreateHashTagOutput,
+} from './dto/create-hashtag.dto';
+import { UpdateHashTagOutput } from './dto/update-hashtag.dto';
 
 @Resolver(() => Hashtag)
 export class HashtagResolver {
   constructor(private readonly hashtagService: HashtagService) {}
 
   @Mutation(() => CreateHashTagOutput)
-  createHashTag(): Promise<CreateHashTagOutput> {
-    return this.hashtagService.createHashTag();
+  createHashTag(
+    @Args('input') createHashTagInput: CreateHashTagInput,
+  ): Promise<CreateHashTagOutput> {
+    return this.hashtagService.createHashTag(createHashTagInput);
+  }
+
+  @Mutation(() => UpdateHashTagOutput)
+  updateHashTag(
+    @Args('postId', { type: () => Int }) postId: number,
+  ): Promise<UpdateHashTagOutput> {
+    return this.hashtagService.updateHashTag(postId);
   }
 
   @Query(() => GetHashTagOutput)
