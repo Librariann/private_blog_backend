0d44fe3cff2aa9a3e3b20ef2c62d1c958aaeaaa0
Librariann <okpc0306@naver.com>
2024-05-04 10:00:00 +0900

    [commit] edit hashtag.service.ts

--- src/hashtag/hashtag.service.ts
+++ src/hashtag/hashtag.service.ts
@@ -3,19 +3,30 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Hashtag } from './entity/hashtag.entity';
 import { Repository } from 'typeorm';
 import { GetHashTagOutput } from './dto/get-hashtag-list.dto';
-import { CreateHashTagOutput } from './dto/create-hashtag.dto';
+import {
+  CreateHashTagInput,
+  CreateHashTagOutput,
+} from './dto/create-hashtag.dto';
+import { Post } from 'src/post/entity/post.entity';
+import { UpdateHashTagOutput } from './dto/update-hashtag.dto';
 
 @Injectable()
 export class HashtagService {
   constructor(
     @InjectRepository(Hashtag)
-    private readonly hastag: Repository<Hashtag>,
+    private readonly hashtag: Repository<Hashtag>,
+
+    @InjectRepository(Post)
+    private readonly post: Repository<Post>,
   ) {}
 
   async getHashTagList(): Promise<GetHashTagOutput> {
     try {
+      const hashtags = await this.hashtag.find();
+
       return {
         ok: true,
+        hashtags,
       };
     } catch (e) {
       return {
@@ -25,8 +36,35 @@ export class HashtagService {
     }
   }
 
-  async createHashTag(): Promise<CreateHashTagOutput> {
+  async createHashTag({
+    hashtag,
+    postId,
+  }: CreateHashTagInput): Promise<CreateHashTagOutput> {
     try {
+      const post = await this.post.findOne({
+        where: {
+          id: postId,
+        },
+      });
+
+      if (!post) {
+        return {
+          ok: false,
+          error: '해당 게시물이 없습니다 다시 시도해 주세요',
+        };
+      }
+
+      const hashtags = hashtag.split(','); //해시태그 분할
+
+      for (hashtag of hashtags) {
+        await this.hashtag.save(
+          this.hashtag.create({
+            hashtag,
+            post,
+          }),
+        );
+      }
+
       return {
         ok: true,
       };
@@ -34,6 +72,23 @@ export class HashtagService {
       return {
         ok: false,
         error: '해시태그를 생성 할 수 없습니다. 관리자에게 문의해주세요',
+      };
+    }
+  }
+
+  async updateHashTag(postId: number): Promise<UpdateHashTagOutput> {
+    try {
+      const hashtags = await this.hashtag.find({
+        where: {
+          postId,
+        },
+      });
+
+      console.log(hashtags);
+    } catch (e) {
+      return {
+        ok: false,
+        error: '해시태그를 업데이트 할 수 없습니다. 관리자에게 문의해주세요',
       };
     }
   }
