3f5f4651865b640470f849e16e9270fff29a397a
Librariann <okpc0306@naver.com>
2024-05-29 10:00:00 +0900

    [commit] edit get-categories.dto.ts

--- src/category/dto/get-categories.dto.ts
+++ src/category/dto/get-categories.dto.ts
@@ -1,9 +1,24 @@
-import { ObjectType, Field } from '@nestjs/graphql';
+import { ObjectType, Field, Int } from '@nestjs/graphql';
 import { Output } from 'src/common/dto/output.dto';
 import { Category } from '../entity/category.entity';
+
+@ObjectType()
+export class CategoryCount {
+  @Field(() => String)
+  categoryTitle: string;
+
+  @Field(() => Int)
+  count: number;
+}
 
 @ObjectType()
 export class GetCategoriesOutput extends Output {
   @Field(() => [Category], { nullable: true })
   categories?: Category[];
+}
+
+@ObjectType()
+export class GetCategoriesCountOutput extends Output {
+  @Field(() => [CategoryCount], { nullable: true })
+  categoryCounts?: CategoryCount[];
 }
