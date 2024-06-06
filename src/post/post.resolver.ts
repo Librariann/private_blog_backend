376de69c628879da0e3a1cfd371f2ed79b009058
Librariann <okpc0306@naver.com>
2024-06-06 10:00:00 +0900

    [commit] edit post.resolver.ts

--- src/post/post.resolver.ts
+++ src/post/post.resolver.ts
@@ -2,13 +2,17 @@ import { Post } from './entity/post.entity';
 import { Resolver, Mutation, Args, Query, Int } from '@nestjs/graphql';
 import { PostService } from './post.service';
 import { CreatePostInput, CreatePostOutput } from './dto/create-post.dto';
-import { GetPostListOutput } from './dto/get-post-list.dto';
+import {
+  getPostListByCategoryIdOutput,
+  GetPostListOutput,
+} from './dto/get-post-list.dto';
 import { EditPostInput, EditPostOutput } from './dto/edit-post.dto';
 import { DeletePostOutput } from './dto/delete-post.dto';
 import { User } from 'src/user/entity/user.entity';
 import { AuthUser } from 'src/auth/auth-user.decorator';
 import { UpdatePostHitsOutput } from './dto/update-post-hits.dto';
 import { GetPostOneOutput } from './dto/get-post-one.dto';
+import { Public } from 'src/auth/public.decorator';
 
 @Resolver(() => Post)
 export class PostResolver {
@@ -50,5 +54,13 @@ export class PostResolver {
     @Args('postId', { type: () => Int }) postId: number,
   ): Promise<GetPostOneOutput> {
     return this.postService.getPostFindOne(postId);
+  }
+
+  @Query(() => getPostListByCategoryIdOutput, { nullable: true })
+  @Public()
+  getPostListByCategoryId(
+    @Args('categoryId', { type: () => Int }) categoryId: number,
+  ): Promise<getPostListByCategoryIdOutput> {
+    return this.postService.getPostListByCategoryId(categoryId);
   }
 }
