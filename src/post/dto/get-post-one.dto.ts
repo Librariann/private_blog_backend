5e5242bf02fd0b9fcdfcbc5503ae0098e7b2c6b4
Librariann <okpc0306@naver.com>
2024-06-23 10:00:00 +0900

    [commit] delete get-post-one.dto.ts

--- src/post/dto/get-post-one.dto.ts
+++
@@ -1,9 +0,0 @@
-import { ObjectType, Field } from '@nestjs/graphql';
-import { Output } from 'src/common/dto/output.dto';
-import { Post } from '../entity/post.entity';
-
-@ObjectType()
-export class GetPostOneOutput extends Output {
-  @Field(() => Post, { nullable: true })
-  post?: Post;
-}
