436e220d65f708b4d4fb98837e5a0f07e58ad49d
Librariann <okpc0306@naver.com>
2024-09-23 10:00:10 +0900

    [edit] add HashtagModule

--- src/post/post.module.ts
+++ src/post/post.module.ts
@@ -5,8 +5,15 @@ import { PostResolver } from './post.resolver';
 import { PostService } from './post.service';
 import { Category } from 'src/category/entity/category.entity';
 import { CategoryModule } from 'src/category/category.module';
+import { Hashtag } from 'src/hashtag/entity/hashtag.entity';
+import { HashtagModule } from 'src/hashtag/hashtag.module';
+
 @Module({
-  imports: [TypeOrmModule.forFeature([Post, Category]), CategoryModule],
+  imports: [
+    TypeOrmModule.forFeature([Post, Category, Hashtag]),
+    CategoryModule,
+    HashtagModule,
+  ],
   providers: [PostResolver, PostService],
   exports: [PostService],
 })
