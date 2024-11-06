a2dde25138e4877addd47de07818ea94a359bd4c
Librariann <okpc0306@naver.com>
2024-11-06 09:15:00 +0900

    update create post dto structure

--- src/post/dto/create-post.dto.ts
+++ src/post/dto/create-post.dto.ts
@@ -1,14 +1,23 @@
 import { PickType, ObjectType, InputType, Int, Field } from '@nestjs/graphql';
-import { Post } from '../entity/post.entity';
+import { Post, PostStatus } from '../entity/post.entity';
 import { Output } from 'src/common/dto/output.dto';
 
 @InputType()
 export class CreatePostInput extends PickType(Post, ['title', 'contents']) {
-  @Field(() => Int)
-  categoryId?: number;
+  @Field(() => Int, { nullable: false })
+  categoryId: number;
 
-  @Field(() => String)
+  @Field(() => String, { nullable: true })
   thumbnailUrl?: string;
+
+  @Field(() => String, { nullable: true })
+  excerpt?: string;
+
+  @Field(() => PostStatus, {
+    nullable: false,
+    defaultValue: PostStatus.PUBLISHED,
+  })
+  postStatus: PostStatus;
 }
 
 @ObjectType()
