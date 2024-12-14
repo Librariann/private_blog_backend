616baaede33d509e1dd8e1e4fcbe03194bc3b93c
Librariann <okpc0306@naver.com>
2024-12-14 10:00:00 +0900

    add PostModule import to CommentModule for dependency injection

--- src/comment/comment.module.ts
+++ src/comment/comment.module.ts
@@ -3,10 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Comment } from './entity/comment.entity';
 import { CommentResolver } from './comment.resolver';
 import { CommentService } from './comment.service';
+import { PostModule } from 'src/post/post.module';
 import { Post } from 'src/post/entity/post.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Comment, Post])],
+  imports: [TypeOrmModule.forFeature([Comment, Post]), PostModule],
   providers: [CommentResolver, CommentService],
   exports: [CommentService],
 })
