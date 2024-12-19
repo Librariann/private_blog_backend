9b3b24aec1c6f0dee0893e58cc2bab5eae539925
Librariann <okpc0306@naver.com>
2024-12-19 10:00:00 +0900

    update comment resolver to support public access and add getCommentList query

--- src/comment/comment.resolver.ts
+++ src/comment/comment.resolver.ts
@@ -1,4 +1,4 @@
-import { Args, Int, Mutation, Resolver } from '@nestjs/graphql';
+import { Args, Int, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { CommentService } from './comment.service';
 import {
   CreateCommentInput,
@@ -9,19 +9,23 @@ import { AuthUser } from 'src/auth/auth-user.decorator';
 import { DeleteCommentOutput } from './dto/delete-comment.dto';
 import { EditCommentInput, EditCommentOutput } from './dto/edit-comment.dto';
 import { Comment } from './entity/comment.entity';
+import { Public } from 'src/auth/public.decorator';
+import { GetCommentOutput } from './dto/get-comment.dto';
 
 @Resolver(() => Comment)
 export class CommentResolver {
   constructor(private readonly commentService: CommentService) {}
 
   @Mutation(() => CreateCommentOutput)
+  @Public()
   createComment(
-    @AuthUser() authUser: User,
+    // @AuthUser() authUser: User,
     @Args('input') createCommentInput: CreateCommentInput,
   ): Promise<CreateCommentOutput> {
-    return this.commentService.createComment(authUser, createCommentInput);
+    return this.commentService.createComment(createCommentInput);
   }
   @Mutation(() => EditCommentOutput)
+  @Public()
   editComment(
     @AuthUser() user: User,
     @Args('input') editCommentInput: EditCommentInput,
@@ -30,7 +34,14 @@ export class CommentResolver {
   }
 
   @Mutation(() => DeleteCommentOutput)
+  @Public()
   deleteComment(@Args('commentId', { type: () => Int }) commentId: number) {
     return this.commentService.deleteComment(commentId);
+  }
+
+  @Query(() => GetCommentOutput)
+  @Public()
+  getCommentList(@Args('postId', { type: () => Int }) postId: number) {
+    return this.commentService.getComments(postId);
   }
 }
