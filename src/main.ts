7f05a50d4699518705a408c58409649ad57ba03d
Librariann <okpc0306@naver.com>
2025-02-19 10:00:10 +0900

    edit cors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors({
-    origin: true,
+    origin: [
+      'http://localhost:3000',
+      'https://private-blog-frontend.vercel.app',
+    ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
