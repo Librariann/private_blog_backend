188c4fd2b67ff0a547bf1e94f451a001d1e1a99a
Librariann <okpc0306@naver.com>
2025-10-13 11:20:00 +0900

    add input validation for comment deletion
    Create DeleteCommentInput DTO with id and commentPassword fields
    to support secure anonymous comment deletion operations.

--- src/comment/dto/delete-comment.dto.ts
+++ src/comment/dto/delete-comment.dto.ts
@@ -1,5 +1,14 @@
-import { ObjectType } from '@nestjs/graphql';
+import { Field, InputType, Int, ObjectType } from '@nestjs/graphql';
 import { Output } from 'src/common/dto/output.dto';
+
+@InputType()
+export class DeleteCommentInput {
+  @Field(() => Int)
+  id: number;
+
+  @Field(() => String)
+  commentPassword: string;
+}
 
 @ObjectType()
 export class DeleteCommentOutput extends Output {}
