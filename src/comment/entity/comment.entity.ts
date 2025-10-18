c965aa54d4fa83d52616f18abd8d62ee14514a2e
Librariann <okpc0306@naver.com>
2025-10-18 13:50:00 +0900

    add password hashing and validation to comment entity
    Implement bcrypt-based password security with automatic hashing on insert/update
    and password verification method for secure anonymous comment authentication.

--- src/comment/entity/comment.entity.ts
+++ src/comment/entity/comment.entity.ts
@@ -1,9 +1,11 @@
 import { CoreEntity } from 'src/common/entity/core.entity';
-import { Entity, Column, ManyToOne } from 'typeorm';
+import { Entity, Column, ManyToOne, BeforeInsert, BeforeUpdate } from 'typeorm';
 import { Field, ObjectType, InputType } from '@nestjs/graphql';
 import { IsString } from 'class-validator';
 import { User } from 'src/user/entity/user.entity';
 import { Post } from 'src/post/entity/post.entity';
+import * as bcrypt from 'bcryptjs';
+import { InternalServerErrorException } from '@nestjs/common';
 
 @InputType('CommentInputType', { isAbstract: true })
 @ObjectType()
@@ -36,4 +38,24 @@ export class Comment extends CoreEntity {
   @Field(() => String)
   @IsString()
   commentPassword: string;
+
+  @BeforeInsert()
+  @BeforeUpdate()
+  async hashPassword(): Promise<void> {
+    if (this.commentPassword) {
+      try {
+        this.commentPassword = await bcrypt.hash(this.commentPassword, 10);
+      } catch (e) {
+        throw new InternalServerErrorException();
+      }
+    }
+  }
+
+  async checkPassword(aPassword: string): Promise<boolean> {
+    try {
+      return await bcrypt.compare(aPassword, this.commentPassword);
+    } catch (e) {
+      throw new InternalServerErrorException();
+    }
+  }
 }
